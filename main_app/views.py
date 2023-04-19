a4b356c7180fe266607f865b9c97ecffb9ecfd1f
adricastro1 <adricastro1196@gmail.com>
2023-04-19 09:28:42 -0400

    deployment prep

--- main_app/views.py
+++ main_app/views.py
@@ -25,10 +25,10 @@ def finches_index(request):
 def finches_detail(request, finch_id):
    finch = Finch.objects.get(id=finch_id)
    id_list = finch.toys.all().values_list('id')
-   toys_cat_doesnt_have = Toy.objects.exclude(id__in=id_list)
+   toys_finch_doesnt_have = Toy.objects.exclude(id__in=id_list)
    feeding_form = FeedingForm()
    return render(request, 'finches/detail.html', {
-      'finch': finch, 'feeding_form': feeding_form, 'toys': toys_cat_doesnt_have
+      'finch': finch, 'feeding_form': feeding_form, 'toys': toys_finch_doesnt_have
       })
 
 def add_feeding(request, finch_id):
